559ba7e244bc21f8ce78c060dd8694e7851e7701
ooishi1000 <karugon64@gmail.com>
2021-12-21 15:52:19 +0900

    Dateテーブルのシーディング他

--- database/seeders/DateSeeder.php
+++ database/seeders/DateSeeder.php
@@ -17,15 +17,19 @@ class DateSeeder extends Seeder
         $datacount = 1; //日付カウントの変数
         $monthcount = 1; //月カウントの変数
 
+        function insert($datacount, $monthcount) {
+            $param = [
+                'date' => $datacount,
+                'month' => $monthcount,
+                'age' => '2020',
+            ];
+            DB::table('dates')->insert($param);
+        }
+
         for($i = 0; $i < 12; $i++) {
             if($monthcount == 1 || $monthcount == 3 || $monthcount == 5 || $monthcount == 7 || $monthcount == 8 || $monthcount == 10 ||  $monthcount ==12) {
                 for($n = 0; $n < 31; $n++){ 
-                        $param = [
-                            'date' => $datacount,
-                            'month' => $monthcount,
-                            'age' => '2020',
-                        ];
-                        DB::table('dates')->insert($param);
+                    insert($datacount, $monthcount);
                         if($datacount < 31){
                             $datacount++;
                         } else {
@@ -34,12 +38,7 @@ class DateSeeder extends Seeder
                 }          
             } elseif ($monthcount == 4 || $monthcount == 6 || $monthcount == 9 ||$monthcount ==  11) {
                 for($n = 0; $n < 30; $n++){ 
-                    $param = [
-                        'date' => $datacount,
-                        'month' => $monthcount,
-                        'age' => '2020',
-                    ];
-                    DB::table('dates')->insert($param);
+                    insert($datacount, $monthcount);
                     if($datacount < 30){
                         $datacount++;
                     } else {
@@ -48,12 +47,7 @@ class DateSeeder extends Seeder
                 }
             } elseif($monthcount == 2) {
                 for($n = 0; $n < 29; $n++){
-                    $param = [
-                        'date' => $datacount,
-                        'month' => $monthcount,
-                        'age' => '2020',
-                    ];
-                    DB::table('dates')->insert($param);
+                    insert($datacount, $monthcount);
                     if($datacount < 29){
                         $datacount++;
                     } else {
@@ -63,5 +57,8 @@ class DateSeeder extends Seeder
             }
             $monthcount++;
         }
+
+        
     }
+    
 }
